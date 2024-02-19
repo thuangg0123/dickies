9bffbf06515e4fb032de160db1f73c6d7593fb06
Thuan Hoang <thuangg456@gmail.com>
2024-02-19 21:20:50 +0700

    add api sorting, filtering

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,4 @@
+const { query } = require('express')
 const Product = require('../models/product')
 const asyncHandler = require('express-async-handler')
 var slugify = require('slugify')
@@ -29,12 +30,38 @@ const getProduct = asyncHandler(async (req, res) => {
 })
 
 const getAllProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find()
-    return res.status(200).json({
-        susccess: products ? true : false,
-        dataProduct: products ? products : 'cannot get products',
-        errorCode: products ? 1 : 0
-    })
+    const queries = { ...req.query }
+    // tách các trường đặc biệt ra khỏi query
+    const excludeField = ['limit', 'sort', 'page', 'fields']
+    excludeField.forEach(element => delete queries[element])
+    // format lại các operators cho đúng cú pháp của mongoose
+    let queryString = JSON.stringify(queries)
+    queryString = queryString.replace(/\b(gte|gt|lt|lte)\b/g, matchedElement => `$${matchedElement}`)
+    const formattedQueries = JSON.parse(queryString)
+    //filtering
+    if (queries.title) {
+        formattedQueries.title = { $regex: queries.title, $options: 'i' }
+    }
+    let queryCommand = Product.find(formattedQueries)
+    //excute query
+
+    //sorting
+    if (req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ')
+        queryCommand = queryCommand.sort(sortBy)
+    }
+
+    queryCommand.then(async (response) => {
+        const counts = await Product.find(formattedQueries).countDocuments()
+        return res.status(200).json({
+            susccess: counts > 0 ? true : false,
+            dataProduct: counts > 0 ? response : 'cannot get products',
+            errorCode: counts > 0 ? 1 : 0,
+            counts: counts
+        })
+    }).catch((err) => {
+        console.log("Error: ", err)
+    });
 })
 
 const updateProduct = asyncHandler(async (req, res) => {
